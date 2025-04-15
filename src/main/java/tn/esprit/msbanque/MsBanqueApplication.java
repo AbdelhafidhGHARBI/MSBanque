6d547a06a08bf9985f2134065ea0b5e3a90fdcac
GHARBI Abdelhafidh <130412760+AbdelhafidhGHARBI@users.noreply.github.com>
2025-04-15 23:01:28 +0100

    first commit

---
+++ src/main/java/tn/esprit/msbanque/MsBanqueApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.msbanque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBanqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBanqueApplication.class, args);
+	}
+
+}
