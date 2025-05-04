415b9bc9ba2ee92f873a0dec6e0d7cd34e48ecb0
GHARBI Abdelhafidh <130412760+AbdelhafidhGHARBI@users.noreply.github.com>
2025-05-04 19:29:42 +0100

    ✨ Amélioration de BanqueRestController : ajout/modification de la logique REST

--- src/main/java/tn/esprit/msbanque/controller/BanqueRestController.java
+++ src/main/java/tn/esprit/msbanque/controller/BanqueRestController.java
@@ -29,7 +29,7 @@ import java.util.Map;
 
 
 @RestController
-@RequestMapping("/api/banques")
+@RequestMapping("/banques")
 @RequiredArgsConstructor
 @CrossOrigin
 public class BanqueRestController {
